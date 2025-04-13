0daaede4b798f92301aa5e88bc7645c55531db79
andres macias <cannibalistico@gmail.com>
2025-04-13 13:27:25 -0600

    feat: first approach

---
+++ src/_nav.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+import {
+  cilBell,
+  cilCalculator,
+  cilChartPie,
+  cilCursor,
+  cilDescription,
+  cilDrop,
+  cilExternalLink,
+  cilNotes,
+  cilPencil,
+  cilPuzzle,
+  cilSpeedometer,
+  cilStar,
+  cilList,
+} from '@coreui/icons'
+import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+
+const _nav = [
+  {
+    component: CNavItem,
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+  },
+
+  {
+    component: CNavItem,
+    name: 'Mis Tareas',
+    to: '/tasks',
+    icon: <CIcon icon={cilList} customClassName="nav-icon" />,
+    badge: {
+      color: 'info',
+      text: '10',
+    },
+  },
+]
+
+export default _nav
